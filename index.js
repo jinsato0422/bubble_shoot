25fddce96ea2d98ac92c51e114305de6f19b58ba
Jin Sato <jinsato0422@gmail.com>
2026-06-20 17:15:24 -0600

    Add enemies class and spawn logic for dynamic enemy generation

--- index.js
+++ index.js
@@ -19,6 +19,28 @@ class Player {
         c.fill();
     }
 }
+class Enemies {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+    }
+
+    update() {
+        this.draw();
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+    }
+}
 
 class Projectile {
     constructor(x, y, radius, color, velocity) {
@@ -49,6 +71,28 @@ const y = canvas.height / 2;
 const player = new Player(x, y, 50, 'blue');
 player.draw();
 const projectiles = [];
+const enemies = [];
+function spawnEnemies() {
+    setInterval(() => {
+        const radius = Math.random() * (30 - 10) + 10;
+        let x;
+        let y;
+        if (Math.random() < 0.5) {
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+            y = Math.random() * canvas.height;
+        } else {
+            x = Math.random() * canvas.width;
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+        }
+        const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
+        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+        const velocity = {
+            x: Math.cos(angle) * 1,
+            y: Math.sin(angle) * 1
+        };
+        enemies.push(new Enemies(x, y, radius, color, velocity));
+    }, 1000);
+}
 
 function animate() {
     requestAnimationFrame(animate);
@@ -56,6 +100,9 @@ function animate() {
     player.draw();
     projectiles.forEach((projectile) => {
         projectile.update();
+    });
+    enemies.forEach((enemy) => {
+        enemy.update();
     });
 }
 
@@ -81,3 +128,4 @@ window.addEventListener('click', (event) => {
 });
 
 animate();
+spawnEnemies();
