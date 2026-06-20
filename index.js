a8c15bac3cec5ead0fea6e8d890c2128607481ed
Jin Sato <jinsato0422@gmail.com>
2026-06-20 16:55:38 -0600

    Implement projectile class and update animation logic for shooting projectiles

--- index.js
+++ index.js
@@ -20,9 +20,64 @@ class Player {
     }
 }
 
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+    }
+
+    update() {
+        this.draw();
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+    }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 50, 'blue');
 player.draw();
-console.log(player);
+const projectiles = [];
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw();
+    projectiles.forEach((projectile) => {
+        projectile.update();
+    });
+}
+
+window.addEventListener('click', (event) => {
+    const angle = Math.atan2(
+        event.clientY - canvas.height / 2,
+        event.clientX - canvas.width / 2
+    );
+
+    const velocity = {
+        x: Math.cos(angle) * 5,
+        y: Math.sin(angle) * 5
+    };
+
+    const projectile = new Projectile(
+        canvas.width / 2,
+        canvas.height / 2,
+        5,
+        'red',
+        velocity
+    );
+    projectiles.push(projectile);
+});
+
+animate();
