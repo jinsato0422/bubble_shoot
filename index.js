b9cee3c4724e1571778eeb731deb01ce7fad7c2a
Jin Sato <jinsato0422@gmail.com>
2026-06-20 17:35:44 -0600

    Change player and projectile color to white for consistency

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ class Projectile {
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player = new Player(x, y, 50, 'blue');
+const player = new Player(x, y, 50, 'white');
 player.draw();
 const projectiles = [];
 const enemies = [];
@@ -149,7 +149,7 @@ window.addEventListener('click', (event) => {
         canvas.width / 2,
         canvas.height / 2,
         5,
-        'red',
+        'white',
         velocity
     );
     projectiles.push(projectile);
